aa8a492cc6fea4bd576d96ba1125e5f2675cde9e
psiqta <123qweasd>
2015-07-27 21:56:27 +0200

    other options

--- src/main/groovy/com/github/erizo/gradle/JcstressPluginExtension.groovy
+++ src/main/groovy/com/github/erizo/gradle/JcstressPluginExtension.groovy
@@ -6,18 +6,44 @@ import org.gradle.api.Project
  * @author jerzykrlk
  */
 class JcstressPluginExtension {
-    def Project project;
+    def Project project
 
     def jcstressDependency = 'com.github.erizo.gradle:org.openjdk.jcstress.jcstress-core:1.0-20150705182407'
 
-    def include = ".*";
-    def includeTests = false;
+    def includeTests = false
+
+    def concurrency, deoptRatio, forks, iterations
+    def jvmArgs, mode, maxStride, minStride, reportDir
+    def cpuCount, regexp, timeMillis, verbose, yield
 
     public JcstressPluginExtension(final Project project) {
-        this.project = project;
+        this.project = project
     }
 
     def buildArgs() {
-        [include]
+        def result = []
+        addParameter(result, '-c', concurrency)
+        addParameter(result, '-deoptRatio', deoptRatio)
+        addParameter(result, '-f', forks)
+        addParameter(result, '-iters', iterations)
+        addParameter(result, '-jvmArgs', jvmArgs)
+        addParameter(result, '-m', mode)
+        addParameter(result, '-maxStride', maxStride)
+        addParameter(result, '-minStride', minStride)
+        addParameter(result, '-r', reportDir)
+        addParameter(result, '-sc', cpuCount)
+        addParameter(result, '-t', regexp)
+        addParameter(result, '-time', timeMillis)
+        addParameter(result, '-v', verbose)
+        addParameter(result, '-yield', yield)
+        result
+    }
+
+    static def addParameter(result, param, value) {
+        if (param == '-v' && value == true) {
+            result << param
+        } else if (value != null) {
+            result << param << value
+        }
     }
 }
