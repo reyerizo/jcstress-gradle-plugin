964725ecc0c67354dcffb6348ae425564cd3170b
Eric Bottard <bottarde@vmware.com>
2020-10-16 15:23:50 +0200

    fix #33 support up-to-date check
    This adds an outputproperty made of the reports directory.
    Given that the JcstressTask inherits from the JavaExec task, input
    properties (which determine what needs to be looked at to decide if
    task is up-to-date) are already supported and contain
    - the java exec classpath (which itselfs changes when user tests change)
    - the java exec args

--- src/main/java/com/github/erizo/gradle/JcstressTask.java
+++ src/main/java/com/github/erizo/gradle/JcstressTask.java
@@ -1,16 +1,20 @@
 package com.github.erizo.gradle;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.JavaExec;
 import org.gradle.api.tasks.Optional;
+import org.gradle.api.tasks.OutputDirectory;
 import org.gradle.api.tasks.options.Option;
 
 public class JcstressTask extends JavaExec {
 
     private String jcstressTestName;
+
+    File reportsDirectory;
 
     @Option(option = "tests", description = "JCstress tests to execute.")
     public void setJcstressTestName(String jcstressTestName) {
@@ -21,6 +25,11 @@ public class JcstressTask extends JavaExec {
     @Optional
     public String getJcstressTestName() {
         return jcstressTestName;
+    }
+
+    @OutputDirectory
+    public File getReportsDirectory() {
+        return reportsDirectory;
     }
 
     public List<String> jcstressArgs() {
